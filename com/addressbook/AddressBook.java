1f462d4eb9b5429451eca168878f2ed502d60e09
rehan7594 <rehansdm94@gmail.com>
2021-04-19 18:13:02 -0700

    Added: Adding person details in addressbook

--- com/addressbook/AddressBook.java
+++ com/addressbook/AddressBook.java
@@ -1,82 +1,16 @@
 package com.addressbook;
 
-import java.util.Scanner;
+/*
+program to create Address Book with person's details
+like first_Name,last_Name,city,state,zip,phoneNumber
+*/
+import java.util.*;
 
-/*@Description- this class represents a address book.
- *contact information  */
-/*@Parameters- taken Contacts class object as variable so that we can
- *  add a new contact to AddressBook*/
+public class AddressBook {
 
-public  class AddressBook {
-	Scanner sc=new Scanner(System.in);
-	static ContactInfo[] contactList=new ContactInfo[15];
-	// To add a new contact to address book by taking values from user
-	int add(int increment)
-	{
-		System.out.println("Enter First Name:");
-		String firstname=sc.next();
-		System.out.println("Enter last name:");
-		String lastname=sc.next();
-		System.out.println("Enter Address:");
-		String address=sc.next();
-		System.out.println("Enter City:");
-		String city=sc.next();
-		System.out.println("Enter State:");
-		String state=sc.next();
-		System.out.println("Enter postal_code:");
-		String zipcode=sc.next();
-		System.out.println("Enter Country-code");
-		String country=sc.next();
-		System.out.println("Enter contact number:");
-		String mob=sc.next();
-		System.out.println("Enter email :");
-		String mail=sc.next();
-		contactList[increment++]=new  ContactInfo(firstname, lastname, address, city, state,zipcode,country,mob,mail);
-		return increment;
-	}
-
-	/*
-	 * To update existing contact details by taking input from user 
-	 * details user wants to edit and also checking if the name entered by user
-	 * exist is Address Book or not
-	 */
-	void Edit(int increment)
-	{
-		int i;
-		for(i=0;i<10;i++)
-		{
-			if(i==increment)
-			{
-
-				System.out.println("Enter First Name:");
-				String firstname=sc.next();
-				System.out.println("Enter last name:");
-				String lastname=sc.next();
-				System.out.println("Enter Address:");
-				String address=sc.next();
-				System.out.println("Enter City:");
-				String city=sc.next();
-				System.out.println("Enter State:");
-				String state=sc.next();
-				System.out.println("Enter postal_code:");
-				String zipcode=sc.next();
-				System.out.println("Enter Country:");
-				String country=sc.next();
-				System.out.println("Enter contact number:");
-				String mob=sc.next();
-				System.out.println("Enter email :");
-				String mail=sc.next();
-				contactList[increment++]=new  ContactInfo(firstname, lastname, address, city, state,zipcode,country,mob,mail);
-				break;
-
-			}	
-		}
-		System.out.println("Your changes replicated to the Book successfully....");
-	}
-	public static void main(String args[]){
-		Scanner sc=new Scanner(System.in);
-		AddressBook ab=new AddressBook();
-		ab.Edit(0); 
-
+	public static void main(String[] args) {
+		System.out.println("person details");
+		ContactInfo c = new ContactInfo("rehan", "kumar", "banglore", "karnataka" , "btm 2nd stage", "rehansdm94@7594.com","560076", 821764632);
+		c.showPersonsDetails();
 	}
 }
--- com/addressbook/ContactInfo.java
+++ com/addressbook/ContactInfo.java
@@ -1,39 +1,89 @@
 package com.addressbook;
+import java.util.*;
+import java.io.*;
 
-/*
- * contact information has first name, last name, number,address
- * state,city,zipcode, age and email
- */
+public class ContactInfo{
 
-public class ContactInfo {
+	// Class-variable declarations
+	private String firstName, lastName, city, state, address, email,zipCode;
+	private long phoneNumber;
 
-	String FIRST_NAME;
-	String LAST_NAME;
-	String ADDRESS;
-	String CITY;
-	String STATE;
-	String POSTAL_CODE;
-	String COUNTRY;
-	String CONTACT_NUMBER;
-	String EMAIL;
-	ContactInfo(String firstname,String lastname,String address,String city,String state,String zipcode,String country,String mob,String mail)
-	{
-		this.FIRST_NAME=firstname;
-		this.LAST_NAME=lastname;
-		this.ADDRESS=address;
-		this.CITY=city;
-		this.STATE=state;
-		this.POSTAL_CODE=zipcode;
-		this.COUNTRY=country;
-		this.CONTACT_NUMBER=mob;
-		this.EMAIL=mail;
+	public ContactInfo(String firstName, String lastName , String city, String state, String address, String email, String zipCode, long phoneNumber){
+
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.city = city;
+		this.state = state;
+		this.address = address;
+		this.email = email;
+		this.zipCode = zipCode;
+		this.phoneNumber = phoneNumber;
 	}
 
-	public String toString() {
-		return String.format(FIRST_NAME+ " "+LAST_NAME+ " " +ADDRESS+ " "+CITY+ " "+STATE+" "+POSTAL_CODE+" "
-				+COUNTRY+" "+CONTACT_NUMBER+" "+EMAIL);
+	// Methods for modifying class-variables
+
+	public void setfirstName(String firstName){
+		this.firstName = firstName;
 	}
+	public void setlastName(String lastName){
+		this.lastName = lastName;
+	}
+	public void setCity(String city){
+		this.city = city;
+	}
+	public void setState(String state){
+		this.state = state;
+	}
+	public void setAddress(String address){
+		this.address = address;
+	}
+	public void setemail(String email){
+		this.email = email;
+	}
+	public void setZipCode(String zipCode){
+		this.zipCode = zipCode;
+	}
+	public void setPhoneNumber(long phoneNumber){
+		this.phoneNumber = phoneNumber;
+	}
+
+	// Methods for displaying class-variables
+	public String getfirstName(){
+		return this.firstName;
+	}
+	public String getlastName(){
+		return this.lastName;
+	}
+	public String getCity(){
+		return this.city;
+	}
+	public String getState(){
+		return this.state;
+	}
+	public String getAddress(){
+		return this.address;
+	}
+	public String getemail(){
+		return this.email;
+	}
+	public String getZipCode(){
+		return this.zipCode;
+	}
+	public long getPhoneNumber(){
+		return this.phoneNumber;
+	}
+	public void showPersonsDetails(){
+
+
+		System.out.println("First name :: "+ this.getfirstName());
+		System.out.println("Last  name :: "+ this.getlastName());
+		System.out.println("Phone Number :: "+ this.getPhoneNumber());
+		System.out.println("City :: " + this.getCity());
+		System.out.println("State :: "+ this.getState());
+		System.out.println("Address :: "+ this.getAddress());
+		System.out.println("Address :: "+ this.getemail());
+		System.out.println("Zip Code :: "+ this.getZipCode());
+	}
+
 
 }
-
-
